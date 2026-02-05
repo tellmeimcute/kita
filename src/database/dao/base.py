cd8e856e3ab7882d2e913e0016fc779eaab1721b
tellmeimcute <tellmeimcute>
2026-02-05 10:10:24 +1000

    DAO bugfixes

--- src/database/dao/base.py
+++ src/database/dao/base.py
@@ -1,9 +1,10 @@
 
 from typing import Sequence, Generic, TypeVar, Any
 
-from sqlalchemy import select, Result, func
+from sqlalchemy import select, Result, func, ColumnElement
 from sqlalchemy.orm import selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from database.models.abstract_model import AbstractModel
 
 T = TypeVar("T", bound=AbstractModel)
@@ -18,12 +19,10 @@ class BaseDao(Generic[T]):
         )
     
     @classmethod
-    async def count(cls, session: AsyncSession, filters: AbstractModel | None = None) -> int:
-        filter_dict = filters.model_dump(exclude_unset=True) if filters else {}
-        
+    async def count(cls, session: AsyncSession, filters: ColumnElement[bool] | None = None) -> int:
         stmt = (
             select(func.count(cls.model.id))
-            .filter_by(**filter_dict)
+            .where(filters)
         )
         count = await session.scalar(stmt)
         return count or 0
@@ -32,16 +31,14 @@ class BaseDao(Generic[T]):
     async def get(
         cls,
         session: AsyncSession,
-        filters: AbstractModel | None = None,
+        filters: ColumnElement[bool] | None = None,
         order_by: Any = None,
         offset: int = 0,
         limit: int = 5
     ) -> Sequence[T]:
-        filter_dict = filters.model_dump(exclude_unset=True) if filters else {}
-
         stmt = (
             select(cls.model)
-            .filter_by(**filter_dict)
+            .where(filters)
             .order_by(order_by)
             .offset(offset)
             .limit(limit)
@@ -52,9 +49,8 @@ class BaseDao(Generic[T]):
     
     @classmethod
     async def get_one_or_none_with_children(
-        cls, session: AsyncSession, child: Any, filters: Any
+        cls, session: AsyncSession, child: Any, filters: ColumnElement[bool]
     ) -> T | None:
-        #filter_dict = filters.model_dump(exclude_unset=True) if filters else {}
         stmt = (
             select(cls.model)
             .options(
