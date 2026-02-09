24495598317626afa50b73a0ffd56d3a2d670815
tellmeimcute <tellmeimcute>
2026-02-09 19:04:28 +1000

    format

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -6,13 +6,6 @@ from aiogram.utils.media_group import MediaGroupBuilder
 from database.models import Media, Suggestion
 
 
-def get_media_group(medias: List[Media], caption: str | None = None) -> MediaGroupBuilder:
-    media_group = MediaGroupBuilder(caption=caption)
-    for media in medias:
-        media_group.add(type=media.filetype, media=media.telegram_file_id)
-    return media_group
-
-
 def message_get_media_and_id(msg: Message) -> Tuple[str, str]:
     if msg.video:
         return "video", msg.video.file_id
@@ -23,6 +16,13 @@ def message_get_media_and_id(msg: Message) -> Tuple[str, str]:
     elif msg.document:
         return "document", msg.document.file_id
     return None, None
+
+
+def get_media_group(medias: List[Media], caption: str | None = None) -> MediaGroupBuilder:
+    media_group = MediaGroupBuilder(caption=caption)
+    for media in medias:
+        media_group.add(type=media.filetype, media=media.telegram_file_id)
+    return media_group
 
 
 def build_album_suggestions(
