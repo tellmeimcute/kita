257eb5ecccebcb93029505477267aaf0289dcc48
tellmeimcute <tellmeimcute>
2026-03-28 19:50:01 +1000

    fix

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,2 +1,10 @@
-from .admin import *
-from .user import *
+from .admin import (
+    admin_suggestion_router,
+    admin_general_router,
+    admin_ban_user_router,
+    admin_mass_message_router,
+)
+from .user import (
+    user_start_router,
+    user_suggestion_router,
+)
