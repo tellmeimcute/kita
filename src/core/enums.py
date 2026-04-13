88b74929a0cfa6f761165b3e9912f022e1d570fa
tellmeimcute <tellmeimcute>
2026-04-13 22:41:39 +1000

    refactor(enums): move UpperStrEnum to core/enums

--- src/core/enums.py
+++ src/core/enums.py
@@ -1,5 +1,10 @@
 from enum import StrEnum, auto
 
+class UpperStrEnum(StrEnum):
+    @staticmethod
+    def _generate_next_value_(name: str, start: int, count: int, last_values: list) -> str:
+        return name
+
 
 class RenderType(StrEnum):
     MESSAGE = auto()
--- src/database/roles.py
+++ src/database/roles.py
@@ -1,10 +1,6 @@
-from enum import Enum, StrEnum, auto
 
-
-class UpperStrEnum(StrEnum):
-    @staticmethod
-    def _generate_next_value_(name: str, start: int, count: int, last_values: list) -> str:
-        return name
+from core.enums import UpperStrEnum
+from enum import auto
 
 
 class UserRole(UpperStrEnum):
