064a740a2b2675f6558e3037e30921d0ac002d54
tellmeimcute <tellmeimcute>
2026-06-23 02:12:51 +1000

    feat: banner now supports media content

--- src/routers/banner_menu/handlers.py
+++ src/routers/banner_menu/handlers.py
@@ -1,5 +1,5 @@
 
-from aiogram.types import Message
+from aiogram.types import Message, Video, Animation
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram_dialog import DialogManager, ShowMode
 from aiogram_dialog.widgets.input import MessageInput
@@ -9,9 +9,6 @@ from dishka.integrations.aiogram_dialog import inject
 
 from core.config import Config, RuntimeConfig
 from core.i18n_translator import Translator
-from core.schemas.message_payload import MessagePayload
-
-from services import NotifierService
 from ui.state_groups import AdminMenuSG, BannerMenuSG
 
 
@@ -21,26 +18,50 @@ async def get_banner_text(
     message_input: MessageInput,
     manager: DialogManager,
     translator: FromDishka[Translator],
-    notifier: FromDishka[NotifierService],
     config: FromDishka[Config],
     runtime_config: FromDishka[RuntimeConfig]
 ):
     builder = InlineKeyboardBuilder()
     btn_text = translator.get_translated_text("channel_banner_btn")
     builder.button(text=btn_text, url=runtime_config.bot_url)
+    markup = builder.as_markup()
 
-    if not message.text:
+    banner_text = message.text or message.caption
+    media = message.video or message.photo or message.animation
+
+    if not banner_text:
         return await manager.switch_to(
             BannerMenuSG.something_wrong,
             show_mode=ShowMode.DELETE_AND_SEND,
         )
-    
-    payload = MessagePayload(
-        i18n_key="channel_banner",
-        i18n_kwargs={"text": message.text},
-        reply_markup=builder.as_markup()
-    )
-    strategy = notifier.send_strategy_factory(config.CHANNEL_ID, payload)
-    await notifier.send(strategy)
-    
+
+    if isinstance(media, list):
+        media = media[0]
+        await message.bot.send_photo(
+            config.CHANNEL_ID,
+            photo=media.file_id,
+            caption=banner_text,
+            reply_markup=markup,
+        )
+    elif isinstance(media, Video):
+        await message.bot.send_video(
+            config.CHANNEL_ID,
+            video=media.file_id,
+            caption=banner_text,
+            reply_markup=markup,
+        )
+    elif isinstance(media, Animation):
+        await message.bot.send_animation(
+            config.CHANNEL_ID,
+            animation=media.file_id,
+            caption=banner_text,
+            reply_markup=markup,
+        )
+    elif not media:
+        await message.bot.send_message(
+            config.CHANNEL_ID,
+            text=banner_text,
+            reply_markup=markup,
+        )
+
     await manager.start(AdminMenuSG.main, show_mode=ShowMode.DELETE_AND_SEND)
