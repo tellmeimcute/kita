973b7e74f0899e51a05563fd4ef2aacb6d2fe6d9
tellmeimcute <tellmeimcute>
2026-07-22 18:58:28 +1000

    remove useless code

--- src/database/models/timestamp.py
+++ src/database/models/timestamp.py
@@ -1,11 +1,10 @@
-from datetime import datetime, timezone
+from datetime import datetime
 from typing import Any, Final
 
 from sqlalchemy import DateTime, Function, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 NOW_FUNC: Final[Function[Any]] = func.timezone("UTC", func.now())
-# NOW_FUNC: Final[Function[Any]] = lambda: datetime.now(timezone.utc)
 
 
 class TimestampMixin:
