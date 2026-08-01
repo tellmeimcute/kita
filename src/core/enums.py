629cd1262d3d065ae7653fe2dc9462de711df7d9
tellmeimcute <tellmeimcute>
2026-08-01 11:20:12 +1000

    remove dead code

--- src/core/enums.py
+++ src/core/enums.py
@@ -10,16 +10,3 @@ class UpperStrEnum(StrEnum):
 class RenderType(StrEnum):
     MESSAGE = auto()
     MEDIAGROUP = auto()
-
-
-class BanAdminAction(StrEnum):
-    BAN = auto()
-    UNBAN = auto()
-
-
-class SettingsMenu(StrEnum):
-    settings_menu = auto()
-    settings_menu_btn = auto()
-    
-    locale_menu = auto()
-    locale_menu_btn = auto()
