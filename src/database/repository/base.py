0550f9d90b3294672784bcf88d44063f27e2d064
tellmeimcute <tellmeimcute>
2026-09-03 00:21:07 +1000

    refactor: remove useless code

--- src/database/repository/base.py
+++ src/database/repository/base.py
@@ -7,7 +7,6 @@ class BaseRepository:
     __slots__ = (
         "_session",
         "_bot_registry",
-        "_bot",
     )
 
     def __init__(
@@ -17,10 +16,7 @@ class BaseRepository:
     ):
         self._session = session
         self._bot_registry = bot_registry
-        self._bot = None
 
     @property
     def bot(self):
-        if self._bot:
-            return self._bot
         return self._bot_registry.get_current()
